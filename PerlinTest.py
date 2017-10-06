263d0c17ecad69af2a4bf11ad87c24dbbdd55d6f
THEJOSHBOX2000\Josh <joshlorenzen49@gmail.com>
2017-10-06 12:23:06 -0700

    Tester now counts how many values were generated out of the correct range

--- PerlinTest.py
+++ PerlinTest.py
@@ -3,12 +3,17 @@ import Perlin
 import math
 from PIL import Image
 
-noise = Perlin.Perlin(500)
+noise = Perlin.Perlin(75)
 img = Image.new('L', (1000, 1000), 255)
 data = img.load()
+outOfRangeCount = 0
 for x in range(1000):
 	for y in range(1000):
-		#print(x, y, math.floor((noise.genNoise(x, y) + 1) * 128))
-		data[x,y] = (math.floor((noise.genNoise(x, y)) * 255),)
+		value = noise.genNoise(x, y)
+		if value < 0 or value > 1:
+			outOfRangeCount += 1
+		data[x,y] = (math.floor(value * 255),)
+
 img.save('image.png', "PNG")
 print("DONE")
+print("Values out of range:", outOfRangeCount)
